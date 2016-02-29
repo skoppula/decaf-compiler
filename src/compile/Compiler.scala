38f9cbd10ccbd4d4ee0b57b191f099ec71e6826d
Austin Liew <ajyliew@mit.edu>
2016-02-28 23:30:16 -0500

    added some walking functions

--- src/compile/Compiler.scala
+++ src/compile/Compiler.scala
@@ -99,20 +99,182 @@ object Compiler {
       * Create the intermediate AST representation + symbol table structures
       * Where all the identifier and semantic checking magic will happen
       */
-    val ast: CommonAST = parse(fileName);
-    println("num children " + ast.getNumberOfChildren());
-    println("root text and type " +  ast.getText() + ' ' + ast.getType());
-    println("first child " + ast.getFirstChild());
-    println("next sibling " + ast.getNextSibling());
-    val main = ast.getNextSibling
-    println("next next sibling " + main.getNextSibling);
-    println("next sibling children" + main.getFirstChild);
-    println(ast.toStringList());
-    println("dsds")
-    println(ast.toStringTree());
-    println(classOf[DecafParserTokenTypes].getFields);
-    println()
-    println(DecafParser._tokenNames.deep.mkString(","))
-    return (ast, null)
+    //val ast: CommonAST = parse(fileName);
+    //println("num children: " + ast.getNumberOfChildren());
+    //println("root text: \"" +  ast.getText() + "\", type: " + ast.getType());
+
+
+    //println("first child: " + ast.getFirstChild());
+    //println("next sibling: " + ast.getNextSibling());
+    //println("next next sibling: " + main.getNextSibling);
+    //println("next sibling children: " + main.getFirstChild);
+    //println(ast.toStringList());
+    //println("")
+    //println(ast.toStringTree());
+    //println(classOf[DecafParserTokenTypes].getFields);
+    //println(DecafParser._tokenNames.deep.mkString(","))
+
+
+/* This is for the normal walk, but node nesting is not too visible with this
+    def funcPre(ast : CommonAST) : Any = {
+      println("Entered " + ast.getText())
+    }
+
+    def funcPost(ast : CommonAST) : Any = {
+      println("Exited " + ast.getText())
+    }
+
+    val ast = Option(parse(fileName));
+
+    ast match {
+      case Some(a) => {
+        walk(a, funcPre, funcPost);
+        return (a, null);
+      }
+      case None => {}
+    }
+
+*/
+
+    def funcPre(ast : CommonAST, a : Any) : Any = {
+      // Below is unsafe, but we just want a quick print for now.
+      val s : String = a.asInstanceOf[String]
+      println(s + ">" + "Entered " + ast.getText())
+      return (s + ">")
+    }
+
+    def funcPost(ast : CommonAST, a : Any) : Any = {
+      val s : String = a.asInstanceOf[String]
+      println(s + "Exited " + ast.getText())
+      return s.substring(0, s.length - 1)
+    }
+
+    val ast = Option(parse(fileName));
+
+    ast match {
+      case Some(a) => {
+        walkExperimental(a, funcPre, funcPost, "");
+        return (a, null);
+      }
+      case None => {}
+    }
+
+
+
+
+    return (null, null)
   }
+
+  def walkPreOrder(ast : CommonAST, funcPre : CommonAST => Any) : Any = {
+    /**
+      * DFS recursion via preorder traversal through the AST. Applies the 
+      * function funcPreOrder on ast, and then recursively calls walk on the child 
+      * (if it exists) and then on the next sibling (if it exists)
+      */
+
+    funcPre(ast)
+
+    // Recursive call on child
+    val child = Option(ast.getFirstChild)
+    child match {
+      case Some(c:CommonAST) => walkPreOrder(c, funcPre)
+      case None => {}
+    }
+
+    // recursive call on the next sibling
+    var sibling = Option(ast.getNextSibling)
+    sibling match {
+      case Some(s:CommonAST) => {
+        walkPreOrder(s, funcPre)
+      }
+      case None => {}
+    }
+  }
+
+  def walkPostOrder(ast : CommonAST, funcPost : CommonAST => Any) : Any = {
+    /**
+      * DFS recursion via postorder traversal through the AST. Recursively calls 
+      * walkPostOrder on the child (if it exists). Then applies funcPost to ast. 
+      * Then recursively calls walkPostOrder on the next sibling (if it exists). 
+      */
+
+    // Recursive call on child
+    val child = Option(ast.getFirstChild)
+    child match {
+      case Some(c:CommonAST) => walkPostOrder(c, funcPost)
+      case None => {}
+    }
+
+    funcPost(ast)
+
+    // recursive call on the next sibling
+    var sibling = Option(ast.getNextSibling)
+    sibling match {
+      case Some(s:CommonAST) => {
+        walkPostOrder(s, funcPost)
+      }
+      case None => {}
+    }
+
+  }
+
+  def walk(ast : CommonAST, funcPre : CommonAST => Any, funcPost : CommonAST => Any) : Any = {
+    /**
+      * DFS recursion via walk through the AST. Applies the function funcPreOrder on 
+      * ast, and then recursively calls walk on the child (if it exists). Then applies 
+      * the function funcPostOrder on ast and then on the next sibling (if it exists). 
+      */
+
+    funcPre(ast)
+
+    // Recursive call on child
+    val child = Option(ast.getFirstChild)
+    child match {
+      case Some(c:CommonAST) => walk(c, funcPre, funcPost)
+      case None => {}
+    }
+
+    funcPost(ast)
+
+    // recursive call on the next sibling
+    var sibling = Option(ast.getNextSibling)
+    sibling match {
+      case Some(s:CommonAST) => {
+        walk(s, funcPre, funcPost)
+      }
+      case None => {}
+    }
+  }
+
+  def walkExperimental(ast : CommonAST, funcPre : (CommonAST, Any) => Any, funcPost : (CommonAST, Any) => Any, arg : Any) : Any = {
+    /**
+      * DFS recursion via walk through the AST. Takes preorder and postorder function
+      * and and an argument to pass into those functions. Probably should not be used
+      * for any actual walking.
+      */
+
+    val resultPre = funcPre(ast, arg)
+
+    // Recursive call on child
+    val child = Option(ast.getFirstChild)
+    child match {
+      case Some(c:CommonAST) => walkExperimental(c, funcPre, funcPost, resultPre)
+      case None => {}
+    }
+
+    val resultPost = funcPost(ast, resultPre)
+
+    // recursive call on the next sibling
+    var sibling = Option(ast.getNextSibling)
+    sibling match {
+      case Some(s:CommonAST) => {
+        walkExperimental(s, funcPre, funcPost, arg)
+      }
+      case None => {}
+    }
+
+    return resultPost
+  }
+
+
 }
