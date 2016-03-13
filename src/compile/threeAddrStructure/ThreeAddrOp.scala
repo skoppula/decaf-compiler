7f1dd7362e04e188596523183fa3b8c6a2b9760f
Skanda Koppula <skanda.koppula@gmail.com>
2016-03-13 18:53:33 -0400

    remove .idea folder

--- src/compile/threeAddrStructure/ThreeAddrOp.scala
+++ src/compile/threeAddrStructure/ThreeAddrOp.scala
@@ -1,5 +1,3 @@
 package compile.threeAddrStructure
 
-class ThreeAddrOp(op: OpTypes.EnumVal, addr1: String, addr2: String, addr3: String) {
-
-}
+case class ThreeAddrOp(op: OpTypes.EnumVal, addr1: String, addr2: String, addr3: String) {}
