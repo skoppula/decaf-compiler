763b7586e382908e03ef8f544cb1f84405f3f715
Skanda Koppula <skanda.koppula@gmail.com>
2016-05-09 15:01:39 -0400

    add the copy copy int optimization

--- src/compile/analysis/ElimCopiesBeforeOp.scala
+++ src/compile/analysis/ElimCopiesBeforeOp.scala
@@ -3,7 +3,7 @@ package compile.analysis
 import compile.cfg.{BasicBlockGenie, CFGUtil, NormalBB}
 import compile.symboltables.SymbolTable
 import compile.tac.TempVariableGenie
-import compile.tac.ThreeAddressCode.{Tac, TacBinOp, TacCopy}
+import compile.tac.ThreeAddressCode.{TacCopyInt, Tac, TacBinOp, TacCopy}
 import compile.util.Util.dprintln
 
 import scala.collection.mutable.ArrayBuffer
@@ -14,8 +14,76 @@ object ElimCopiesBeforeOp {
     val bbIDs : Set[String] = CFGUtil.cfgBBs(methodStart, List())
     for(bbID <- bbIDs) {
       elimFromBBBinOp(BasicBlockGenie.idToBBReference(bbID), tempGenie)
-      elimFromBBUnOp(BasicBlockGenie.idToBBReference(bbID), tempGenie)
+      elimFromBBTacCopyIntTacCopy(BasicBlockGenie.idToBBReference(bbID), tempGenie)
     }
+  }
+
+  def elimFromBBTacCopyIntTacCopy(bb : NormalBB, tempGenie : TempVariableGenie) {
+    /*
+    optimization for when:
+    TacCopyInt(T1,0)
+    TacCopy(A,T1)
+    ->
+    TacCopyInt(A,0)
+     */
+
+    val num_instrs = bb.instrs.size
+    if(num_instrs < 2) return
+    else {
+      val newInstrs = ArrayBuffer.empty[Tac]
+
+      var i = 0
+      while(i < num_instrs) {
+        if(i < num_instrs - 1) {
+          val instr1 = bb.instrs(i)
+          val instr2 = bb.instrs(i + 1)
+          if (instr1.isInstanceOf[TacCopyInt] && instr2.isInstanceOf[TacCopy]) {
+
+            val instr1TCI = instr1.asInstanceOf[TacCopyInt]
+            val instr2TC = instr2.asInstanceOf[TacCopy]
+            val i1_addr1 = instr1TCI.addr1
+            val i1_num = instr1TCI.int
+            val i2_addr1 = instr2TC.addr1
+            val i2_addr2 = instr2TC.addr2
+
+            // TODO would changes by previous bin op optimization mess up DCE_out? i don't think so...
+            val varAndST1 = DCE.getSymbolAndTable(i1_addr1, bb.symbolTable)
+
+            var tempIsUsed = false
+            for(tac <- bb.instrs.slice(i+2,num_instrs)) {
+              val uses = DCE.usePerTac(Set.empty[(String, SymbolTable)], tac, bb.symbolTable)
+              val used_in_tac1 = uses.contains(varAndST1)
+              tempIsUsed = tempIsUsed || used_in_tac1
+            }
+
+            if(bb.dceOut.contains(varAndST1) || tempIsUsed) {
+              newInstrs.append(bb.instrs(i))
+              i += 1
+            } else {
+              // do the TACCopy substitution
+              if (i1_addr1 == i2_addr2) {
+                newInstrs.append(TacCopyInt(tempGenie.generateTacNumber(), i2_addr1, i1_num))
+                i = i + 2
+              } else {
+                newInstrs.append(bb.instrs(i))
+                i += 1
+              }
+            }
+
+          } else {
+            newInstrs.append(bb.instrs(i))
+            i += 1
+          }
+        } else {
+          newInstrs.append(bb.instrs(i))
+          i += 1
+        }
+      }
+
+      bb.instrs.clear()
+      bb.instrs ++= newInstrs
+    }
+
   }
 
   def elimFromBBBinOp(bb : NormalBB, tempGenie : TempVariableGenie) {
@@ -31,7 +99,6 @@ object ElimCopiesBeforeOp {
           val instr2 = bb.instrs(i + 1)
           val instr3 = bb.instrs(i + 2)
           if (instr1.isInstanceOf[TacCopy] && instr2.isInstanceOf[TacCopy] && instr3.isInstanceOf[TacBinOp]) {
-            dprintln("\tFound case where I can replace stuff!!!")
 
             val instr1TC = instr1.asInstanceOf[TacCopy]
             val instr2TC = instr2.asInstanceOf[TacCopy]
@@ -55,15 +122,12 @@ object ElimCopiesBeforeOp {
               tempIsUsed = tempIsUsed || used_in_tac1 || used_in_tac2
             }
 
-            dprintln("\tis either of the temps used??" + tempIsUsed)
-
             if(bb.dceOut.contains(varAndST1) || bb.dceOut.contains(varAndST2) || tempIsUsed) {
               newInstrs.append(bb.instrs(i))
               i += 1
             } else {
               // do the TACCopy substitution
               if (instr1_addr1 == instr3_addr2 && instr2_addr1 == instr3_addr3) {
-                dprintln("\taddresses check out!!!")
                 if(instr1_addr1 == instr2_addr2) {
                   /*
                   case when:
