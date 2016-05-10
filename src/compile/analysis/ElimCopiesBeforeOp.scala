3d5db001a82bc6aea192aded831d6a24cd2f42d0
Skanda Koppula <skanda.koppula@gmail.com>
2016-05-10 02:22:21 -0400

    finish microoptimizations

--- src/compile/analysis/ElimCopiesBeforeOp.scala
+++ src/compile/analysis/ElimCopiesBeforeOp.scala
@@ -15,6 +15,147 @@ object ElimCopiesBeforeOp {
     for(bbID <- bbIDs) {
       elimFromBBBinOp(BasicBlockGenie.idToBBReference(bbID), tempGenie)
       elimFromBBTacCopyIntTacCopy(BasicBlockGenie.idToBBReference(bbID), tempGenie)
+      elimFromBBTacCopyIntBinOp(BasicBlockGenie.idToBBReference(bbID), tempGenie)
+      elimFromBBTacCopyBinOp(BasicBlockGenie.idToBBReference(bbID), tempGenie)
+      elimFromBBTacCopyIntBinOp(BasicBlockGenie.idToBBReference(bbID), tempGenie)
+    }
+  }
+
+  def elimFromBBTacCopyBinOp(bb : NormalBB, tempGenie : TempVariableGenie): Unit = {
+    /*
+    optimization for when:
+    TacCopy(T1,B)
+    TacBinOp(A,T1,C)
+    ->
+    TacBinOp(A,B,C)
+     */
+    val num_instrs = bb.instrs.size
+    if(num_instrs < 2) return
+    else {
+      val newInstrs = ArrayBuffer.empty[Tac]
+
+      var i = 0
+      while(i < num_instrs) {
+        if(i < num_instrs - 1) {
+          val instr1 = bb.instrs(i)
+          val instr2 = bb.instrs(i + 1)
+          if (instr1.isInstanceOf[TacCopy] && instr2.isInstanceOf[TacBinOp]) {
+
+            val instr1TC = instr1.asInstanceOf[TacCopy]
+            val instr2BO = instr2.asInstanceOf[TacBinOp]
+            val instr1_addr1 = instr1TC.addr1
+            val instr1_addr2 = instr1TC.addr2
+            val instr2_addr2 = instr2BO.addr2
+            val instr2_addr3 = instr2BO.addr3
+
+            // TODO maybe should check that temps aren't used again???
+            val varAndST1 = DCE.getSymbolAndTable(instr1_addr1, bb.symbolTable)
+
+            var tempIsUsed = false
+            for(tac <- bb.instrs.slice(i+2,num_instrs)) {
+              val uses = DCE.usePerTac(Set.empty[(String, SymbolTable)], tac, bb.symbolTable)
+              val used_in_tac1 = uses.contains(varAndST1)
+              tempIsUsed = tempIsUsed || used_in_tac1
+            }
+
+            if(bb.dceOut.contains(varAndST1) || tempIsUsed) {
+              newInstrs.append(bb.instrs(i))
+              i += 1
+            } else {
+              // do the TACCopy substitution
+              if (instr1_addr1 == instr2_addr2) {
+                newInstrs.append(TacBinOp(tempGenie.generateTacNumber(), instr2BO.addr1, instr1_addr2, instr2BO.op, instr2_addr3))
+                i = i + 2
+              } else if (instr1_addr1 == instr2_addr3) {
+                newInstrs.append(TacBinOp(tempGenie.generateTacNumber(), instr2BO.addr1, instr2_addr2, instr2BO.op, instr1_addr2))
+                i = i + 2
+              } else {
+                newInstrs.append(bb.instrs(i))
+                i += 1
+              }
+            }
+
+          } else {
+            newInstrs.append(bb.instrs(i))
+            i += 1
+          }
+        } else {
+          newInstrs.append(bb.instrs(i))
+          i += 1
+        }
+      }
+
+      bb.instrs.clear()
+      bb.instrs ++= newInstrs
+    }
+  }
+
+  def elimFromBBTacCopyIntBinOp(bb : NormalBB, tempGenie : TempVariableGenie): Unit = {
+    /*
+    optimization for when:
+    TacCopyInt(T1,0)
+    TacBinOp(A,T1,C)
+    ->
+    TacBinOp(A,.C0, C)
+     */
+    val num_instrs = bb.instrs.size
+    if(num_instrs < 2) return
+    else {
+      val newInstrs = ArrayBuffer.empty[Tac]
+
+      var i = 0
+      while(i < num_instrs) {
+        if(i < num_instrs - 1) {
+          val instr1 = bb.instrs(i)
+          val instr2 = bb.instrs(i + 1)
+          if (instr1.isInstanceOf[TacCopyInt] && instr2.isInstanceOf[TacBinOp]) {
+
+            val instr1TC = instr1.asInstanceOf[TacCopyInt]
+            val instr2BO = instr2.asInstanceOf[TacBinOp]
+            val instr1_addr1 = instr1TC.addr1
+            val instr1_num = instr1TC.int
+            val instr2_addr2 = instr2BO.addr2
+            val instr2_addr3 = instr2BO.addr3
+
+            // TODO maybe should check that temps aren't used again???
+            val varAndST1 = DCE.getSymbolAndTable(instr1_addr1, bb.symbolTable)
+
+            var tempIsUsed = false
+            for(tac <- bb.instrs.slice(i+2,num_instrs)) {
+              val uses = DCE.usePerTac(Set.empty[(String, SymbolTable)], tac, bb.symbolTable)
+              val used_in_tac1 = uses.contains(varAndST1)
+              tempIsUsed = tempIsUsed || used_in_tac1
+            }
+
+            if(bb.dceOut.contains(varAndST1) || tempIsUsed) {
+              newInstrs.append(bb.instrs(i))
+              i += 1
+            } else {
+              // do the TACCopy substitution
+              if (instr1_addr1 == instr2_addr2) {
+                newInstrs.append(TacBinOp(tempGenie.generateTacNumber(), instr2BO.addr1, ".C" + instr1_num, instr2BO.op, instr2_addr3))
+                i = i + 2
+              } else if (instr1_addr1 == instr2_addr3) {
+                newInstrs.append(TacBinOp(tempGenie.generateTacNumber(), instr2BO.addr1, instr2_addr2, instr2BO.op, ".C" + instr1_num))
+                i = i + 2
+              } else {
+                newInstrs.append(bb.instrs(i))
+                i += 1
+              }
+            }
+
+          } else {
+            newInstrs.append(bb.instrs(i))
+            i += 1
+          }
+        } else {
+          newInstrs.append(bb.instrs(i))
+          i += 1
+        }
+      }
+
+      bb.instrs.clear()
+      bb.instrs ++= newInstrs
     }
   }
 
@@ -141,7 +282,6 @@ object ElimCopiesBeforeOp {
                 }
                 i = i + 3
               } else if (instr2_addr1 == instr3_addr2 && instr1_addr1 == instr3_addr2) {
-                dprintln("\taddresses check out!!!")
                 if(instr1_addr1 == instr2_addr2) {
                   /*
                   case when:
